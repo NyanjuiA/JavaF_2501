99784a47a3f02603bcb90f3e6b72fcc9df25ede6
Nyanjui <nyanjuiarthur@gmail.com>
2025-10-29 09:42:45 +0300

    Create the repository for the Java Fundamentals module

---
+++ src/sess01_intro2java/HelloWorld.java
@@ -0,0 +1,14 @@
+package sess01_intro2java;
+
+/**
+ * First Java program to display "Hello World" on the console/terminal
+ * 
+ * @author a.nyanjui
+ */
+public class HelloWorld
+{
+    public void main(String[]args)
+    {
+        System.out.println("Hello World!");
+    }
+}
