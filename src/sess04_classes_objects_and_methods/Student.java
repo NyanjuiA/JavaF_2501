383d35d05fa0d324fdfb9a2111753d876cad1444
Nyanjui <nyanjuiarthur@gmail.com>
2025-11-07 09:20:33 +0300

    Add code to demonstrate classes, objects and methods

---
+++ src/sess04_classes_objects_and_methods/Student.java
@@ -0,0 +1,46 @@
+package sess04_classes_objects_and_methods;
+
+/**
+ * Java class to model a student object. 
+ * 
+ * @author a.nyanjui
+ */
+public class Student
+{
+    String studentID;
+    String name;
+    int age;
+    char gender;
+    
+    // no argument constructor
+
+    public Student()
+    {
+        this.studentID = "";
+        this.name = "";
+        this.age = 0;
+        this.gender = 'F';
+    }
+    
+    // Student initialiser
+    public void initialiseStudent()
+    {
+        this.studentID = "Student0001";
+        this.name = "Student One";
+        this.age = 18;
+    }
+    
+    // Method to display the student's details
+    public void displayDetails()
+    {
+        System.out.println("Student Details");
+        System.out.println("-".repeat(50));
+        System.out.println("Student ID: " + this.studentID);
+        System.out.println("Student's Name: " + this.name);
+        System.out.println("Student's age: " + this.age);
+        System.out.println("Student's gender: " + (this.gender == 'm' || 
+                this.gender == 'M'? "Male":"Female"));
+        System.out.println("-".repeat(50));
+    }
+    
+}
---
+++ src/sess04_classes_objects_and_methods/StudentDemo.java
@@ -0,0 +1,22 @@
+package sess04_classes_objects_and_methods;
+
+/**
+ * Java program to demonstrate creating Student objects and invoking their
+ * methods.
+ * 
+ * @author a.nyanjui
+ */
+public class StudentDemo
+{
+    // main method begins program execution
+    public static void main(String[] args)
+    {
+        Student s1 = new Student();// Declare and instantiate a student object
+        
+        // Initialise the student object
+        s1.initialiseStudent();
+        
+        // Display the student's details
+        s1.displayDetails();
+    }
+}
